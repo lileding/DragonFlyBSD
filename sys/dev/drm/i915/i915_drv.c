cdc35adbebc3b1d1671b961259bc58cbe50392ac
LI Leding <lileding@gmail.com>
2026-07-16 01:05:00 +0800

    drm: scope syncobj timeline support to nvgpu
    Do not advertise DRIVER_SYNCOBJ_TIMELINE from amdgpu or i915 in the nvgpu development branch.  The timeline ABI is maintained by the nvgpu/nvdrm shim and should not change unrelated driver semantics.
    
    Also add the aggregate nvgsp_570 firmware module to the DRM module traversal so nativekernel/installkernel installs it with drm.ko and nvgpu.ko.

--- sys/dev/drm/i915/i915_drv.c
+++ sys/dev/drm/i915/i915_drv.c
@@ -3282,8 +3282,7 @@ static struct drm_driver driver = {
 	 */
 	.driver_features =
 	    DRIVER_HAVE_IRQ | DRIVER_IRQ_SHARED | DRIVER_GEM | DRIVER_PRIME |
-	    DRIVER_RENDER | DRIVER_MODESET | DRIVER_ATOMIC | DRIVER_SYNCOBJ |
-	    DRIVER_SYNCOBJ_TIMELINE,
+	    DRIVER_RENDER | DRIVER_MODESET | DRIVER_ATOMIC | DRIVER_SYNCOBJ,
 	.release = i915_driver_release,
 	.open = i915_driver_open,
 	.lastclose = i915_driver_lastclose,
