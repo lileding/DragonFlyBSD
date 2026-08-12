7df005b995b457073499e74335ee462e0d5bf1f5
LI Leding <lileding@gmail.com>
2026-08-12 13:39:34 +0800

    vmm: test software irqchip delivery
    Exercise the guest-visible delivery paths rather than only irqchip state round trips.\n\nThe LAPIC regression sends KVM_SIGNAL_MSI to a protected-mode guest. The IOAPIC regression routes KVM_IRQ_LINE pin 4 to an IDT handler. Both handlers EOI and halt.

--- test/kvm/kvm_ioapic_test.c
+++ test/kvm/kvm_ioapic_test.c
@@ -1,7 +1,8 @@
 /*-
  * SPDX-License-Identifier: BSD-2-Clause
  *
- * Verifies in-kernel x86 IOAPIC MMIO from a 32-bit protected-mode guest.
+ * Verifies in-kernel x86 IOAPIC MMIO and fixed GSI delivery from a 32-bit
+ * protected-mode guest.
  */
 #include <sys/ioctl.h>
 #include <sys/mman.h>
@@ -19,9 +20,15 @@
 
 #define KVM_IOAPIC_MEMORY_SIZE	0x4000U
 #define KVM_IOAPIC_CODE_OFFSET	0x1000U
+#define KVM_IOAPIC_HANDLER_OFFSET	0x0200U
+#define KVM_IOAPIC_RESULT_OFFSET	0x0300U
+#define KVM_IOAPIC_IDT_OFFSET		0x1800U
 #define KVM_IOAPIC_GDT_OFFSET	0x3000U
 #define KVM_IOAPIC_ADDRESS	0xfec00000U
 #define KVM_IOAPIC_VERSION	0x00170011U
+#define KVM_IOAPIC_GSI		4U
+#define KVM_IOAPIC_VECTOR		0x41U
+#define KVM_IOAPIC_SVR		0x0f0U
 
 static void
 kvm_ioapic_set_segment(struct kvm_segment *segment, uint16_t selector,
@@ -57,11 +64,14 @@ kvm_ioapic_set_protected_entry(int vcpu_fd)
 	sregs.efer = 0;
 	sregs.gdt.base = KVM_IOAPIC_GDT_OFFSET;
 	sregs.gdt.limit = 3 * sizeof(uint64_t) - 1;
+	sregs.idt.base = KVM_IOAPIC_IDT_OFFSET;
+	sregs.idt.limit = 256 * sizeof(uint64_t) - 1;
 	if (ioctl(vcpu_fd, KVM_SET_SREGS, &sregs) != 0)
 		err(1, "KVM_SET_SREGS");
 	bzero(&regs, sizeof(regs));
 	regs.rip = KVM_IOAPIC_CODE_OFFSET;
-	regs.rflags = 0x2;
+	regs.rsp = 0x2ff0;
+	regs.rflags = 0x202;
 	if (ioctl(vcpu_fd, KVM_SET_REGS, &regs) != 0)
 		err(1, "KVM_SET_REGS");
 }
@@ -70,6 +80,7 @@ static void
 kvm_ioapic_write_code(uint8_t *memory)
 {
 	uint64_t *gdt;
+	uint64_t *idt;
 	static const uint8_t code[] = {
 		0xbf, 0x00, 0x00, 0xc0, 0xfe,	/* mov $0xfec00000,%edi */
 		0x31, 0xdb,				/* xor %ebx,%ebx */
@@ -78,13 +89,27 @@ kvm_ioapic_write_code(uint8_t *memory)
 		0xb8, 0x01, 0x00, 0x00, 0x00,	/* mov $1,%eax */
 		0x89, 0x07,				/* mov %eax,(%edi) */
 		0x8b, 0x47, 0x10,			/* mov 0x10(%edi),%eax */
+		0xfb,					/* sti */
+		0xf4,					/* hlt */
+		0xeb, 0xfd				/* jmp hlt */
+	};
+	static const uint8_t handler[] = {
+		0xc6, 0x05, 0x00, 0x03, 0x00, 0x00, 0x7a,
+						/* movb $0x7a,0x300 */
+		0xb8, 0x00, 0x00, 0x00, 0x00,	/* mov $0,%eax */
+		0xbf, 0xb0, 0x00, 0xe0, 0xfe,	/* mov $0xfee000b0,%edi */
+		0x89, 0x07,				/* mov %eax,(%edi) */
 		0xf4					/* hlt */
 	};
 
 	gdt = (uint64_t *)(memory + KVM_IOAPIC_GDT_OFFSET);
 	gdt[1] = 0x00cf9b000000ffffULL;
 	gdt[2] = 0x00cf93000000ffffULL;
+	idt = (uint64_t *)(memory + KVM_IOAPIC_IDT_OFFSET);
+	idt[KVM_IOAPIC_VECTOR] = KVM_IOAPIC_HANDLER_OFFSET |
+	    ((uint64_t)0x08 << 16) | ((uint64_t)0x8e << 40);
 	bcopy(code, memory + KVM_IOAPIC_CODE_OFFSET, sizeof(code));
+	bcopy(handler, memory + KVM_IOAPIC_HANDLER_OFFSET, sizeof(handler));
 }
 
 static void
@@ -107,6 +132,8 @@ main(void)
 {
 	struct kvm_userspace_memory_region memory_region;
 	struct kvm_irqchip irqchip;
+	struct kvm_irq_level irq_line;
+	struct kvm_lapic_state lapic;
 	struct kvm_regs regs;
 	struct kvm_run *run;
 	uint8_t *guest_memory;
@@ -115,6 +142,7 @@ main(void)
 	int vm_fd;
 	int vcpu_fd;
 	int run_size;
+	uint32_t value;
 
 	control_fd = open("/dev/kvm", O_RDWR | O_CLOEXEC);
 	if (control_fd < 0)
@@ -141,6 +169,13 @@ main(void)
 	vcpu_fd = ioctl(vm_fd, KVM_CREATE_VCPU, 0);
 	if (vcpu_fd < 0)
 		err(1, "KVM_CREATE_VCPU");
+	bzero(&lapic, sizeof(lapic));
+	if (ioctl(vcpu_fd, KVM_GET_LAPIC, &lapic) != 0)
+		err(1, "KVM_GET_LAPIC");
+	value = 0x1ffU;
+	bcopy(&value, lapic.regs + KVM_IOAPIC_SVR, sizeof(value));
+	if (ioctl(vcpu_fd, KVM_SET_LAPIC, &lapic) != 0)
+		err(1, "KVM_SET_LAPIC");
 	kvm_ioapic_set_protected_entry(vcpu_fd);
 	run_size = ioctl(vm_fd, KVM_GET_VCPU_MMAP_SIZE);
 	if (run_size != 2 * getpagesize())
@@ -165,6 +200,22 @@ main(void)
 		errx(1, "IOAPIC id %#x", (uint32_t)regs.rbx);
 	if ((uint32_t)regs.rax != KVM_IOAPIC_VERSION)
 		errx(1, "IOAPIC version %#x", (uint32_t)regs.rax);
+	irqchip.chip.ioapic.redirtbl[KVM_IOAPIC_GSI].bits =
+	    KVM_IOAPIC_VECTOR;
+	if (ioctl(vm_fd, KVM_SET_IRQCHIP, &irqchip) != 0)
+		err(1, "KVM_SET_IRQCHIP redirection");
+	irq_line.irq = KVM_IOAPIC_GSI;
+	irq_line.level = 1;
+	if (ioctl(vm_fd, KVM_IRQ_LINE, &irq_line) != 0)
+		err(1, "KVM_IRQ_LINE assert");
+	irq_line.level = 0;
+	if (ioctl(vm_fd, KVM_IRQ_LINE, &irq_line) != 0)
+		err(1, "KVM_IRQ_LINE deassert");
+	kvm_ioapic_run(vcpu_fd);
+	if (run->exit_reason != KVM_EXIT_HLT)
+		err(1, "expected HLT after GSI, got %u", run->exit_reason);
+	if (guest_memory[KVM_IOAPIC_RESULT_OFFSET] != 0x7a)
+		err(1, "IOAPIC GSI handler did not run");
 	if (munmap(run_mapping, run_size) != 0 || close(vcpu_fd) != 0 ||
 	    close(vm_fd) != 0 ||
 	    munmap(guest_memory, KVM_IOAPIC_MEMORY_SIZE) != 0 ||
